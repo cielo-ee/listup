7725c8fbb218dd2c141dab3343374b9f65cb5476
cielo-ee <cielo.ee+github@gmail.com>
2014-07-26 22:28:32 +0900

    first commit

---
+++ listup.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use CGI;
+
+my $filename = "list.tsv";
+
+my $fh;
+my $q  = new CGI;
+
+print $q->header;
+print $q->start_html;
+
+if( -f $filename){
+		open $fh,"<",$filename or die "file open failed!\n";
+}
+else{
+		exit;
+}
+
+
+while (my $line = <$fh>){
+		chomp $line;
+		my @items = split(/\t/,$line);
+		print join("\t",@items);
+		print "\n";
+}
+
+print $q->end_html;
